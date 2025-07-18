2f0a2056ae2f305e2e09b8effb7949212670545f
Harshitaa Sanwal <119100652+Harshitaa109@users.noreply.github.com>
2025-07-18 11:00:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ Connected to MongoDB");
     app.listen(PORT, () => {
